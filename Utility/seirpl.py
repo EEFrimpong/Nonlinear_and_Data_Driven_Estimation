87ac749ef6d746790e980750a6dbf64c0ab5a1e9
EEFrimpong <nana.emma.ef@gmail.com>
2025-11-22 20:38:23 -0800

    seirpl.py

--- Utility/seirpl.py
+++ Utility/seirpl.py
@@ -12,10 +12,10 @@ import pybounds
 # Epidemiological parameters (replace drone parameters)
 
 
-mu    = 0.0000414       # birth/death rate (example, per year)
-beta  = 0.5            # transmission rate
-sigma = 0.2        # exposed -> infectious rate
-gamma = 0.4       # recovery rate
+mu    = 3e-05       # birth/death rate (example, per year)
+beta  = 0.63219          # transmission rate
+sigma = 0.21801    # exposed -> infectious rate
+gamma = 0.1   # recovery rate
 N     = 1_000_000      # total population (assumed constant)
 
 
@@ -187,7 +187,7 @@ def simulate_seir(f, h, tsim_length=365, dt=1.0, measurement_names=None, rterm=1
     cost = I_error**2 + R_error**2
 
     simulator.mpc.set_objective(mterm=cost, lterm=cost)
-    simulator.mpc.set_rterm(u1=0.001, u2=0.3, u3=0.005)
+    simulator.mpc.set_rterm(u1=0.15721, u2=0.002, u3=0.04924)
 
     # Simulate
     t_sim, x_sim, u_sim, y_sim = simulator.simulate(
