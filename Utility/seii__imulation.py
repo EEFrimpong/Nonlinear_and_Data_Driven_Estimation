c337fae22a3ae899078ecdf2940627385106b7e9
EEFrimpong <nana.emma.ef@gmail.com>
2025-11-24 22:28:54 -0800

    Update seii__imulation.py

--- Utility/seii__imulation.py
+++ Utility/seii__imulation.py
@@ -57,7 +57,7 @@ class F(object):
             d(beta_eff)/dt = beta0 * (-ε * 2π/T) sin(2π t/T) (1 - u1)
             dσ/dt          = 0
             dt/dt          = 0
-            dC/dt          = lambda_inf
+            dC/dt          = 0#lambda_inf
         """
 
         if return_state_names:
@@ -79,7 +79,7 @@ class F(object):
         u3 = u_vec[2]     # treatment
 
         # Force of infection using beta_eff and control u1
-        lambda_inf = -beta_eff * (1.0 - u1) * S * I / self.N
+        lambda_inf = beta_eff * (1.0 - u1) * S * I / self.N
 
         # SEIR equations with controls and sigma as a state
         dS_dt = self.mu * self.N - lambda_inf - u2 * S - self.mu * S
